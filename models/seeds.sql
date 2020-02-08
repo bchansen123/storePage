fb659878eb79f0a3c2c3e15516a7ee4221f74896
Donavan Phothirath <donavan66@gmail.com>
2020-02-08 11:37:34 -0700

    added seeds file

---
+++ models/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO songs (category_id, product_name, department_name, image_link, price)
+values
+(1, "Sony Television", "Electronics", "something.jpg", 2000.00),
+(2, "Bannanas", "Food", "this.jpg", 3.99),
+(3, "To Kill a Mockingbird", "Books", "another.jpg", 5.99),
+(1, "Camera", "Electronics", "thing.jpg", 99.99)
