7913deb2e2cb01fb82d5043c2ca66233b8554cfb
bchansen <bchansen@live.com>
2020-02-08 14:12:47 -0700

    Added item_description to the table database

--- models/products.js
+++ models/products.js
@@ -1,50 +1,54 @@
-var Sequelize = require("sequelize");
-var sequelize = require("../config/connection.js");
+module.exports = function(sequelize, Sequelize) {
 
+  var Product = sequelize.define("Product", {
 
-var Product = sequelize.define("product", {
+    category_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    },
 
-  category_id: {
-    type: Sequelize.Integer,
-    allowNull: false,
-    validate: {
-      len: [1, 140]
+    product_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    },
+
+    department_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    },
+
+    image_link: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    },
+
+    item_description: {
+      type: Sequelize.TEXT,
+      allowNull: true,
+    },
+
+    price: {
+      type: Sequelize.DECIMAL,
+      allowNull: false,
+      isDecimal: true,
     }
-  },
-
-  product_name: {
-    type: Sequelize.String,
-    allowNull: false,
-    validate: {
-      len: [1, 140]
-    }
-  },
-
-  department_name: {
-    type: Sequelize.String,
-    allowNull: false,
-    validate: {
-      len: [1, 140]
-    }
-  },
-
-  image_link: {
-    type: Sequelize.String,
-    allowNull: false,
-    validate: {
-      len: [1, 140]
-    }
-  },
-
-  price: {
-    type: Sequelize.Decimal,
-    allowNull: false,
-    isDecimal: true,
-  }
-});
+  });
 
 
-Product.sync();
+  return Product;
+  };
 
 // Makes the Chirp Model available for other files (will also create a table)
-module.exports = Product;
+
