e23f0e705d196b6979967ac49532fe6c79be64b0
Max Power <dbroncosfan@gmail.com>
2020-02-12 13:21:06 -0700

    removed re-added code from merge conflicts

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,8 @@
 var db = require("../models");
-module.exports = function (app) {
+module.exports = function(app) {
   // Load index page
-  app.get("/", function (req, res) {
-
-    db.Product.findAll({ raw: true }).then(function (dbProduct) {
+  app.get("/", function(req, res) {
+    db.Product.findAll({ raw: true }).then(function(dbProduct) {
       var hbsObject = {
         products: dbProduct
       };
@@ -12,8 +11,10 @@ module.exports = function (app) {
     });
   });
 
-  app.get("/electronics", function (req, res) {
-    db.Product.findAll({ raw: true, where: { category_id: 1 } }).then(function (dbProduct) {
+  app.get("/electronics", function(req, res) {
+    db.Product.findAll({ raw: true, where: { category_id: 1 } }).then(function(
+      dbProduct
+    ) {
       var hbsObject = {
         products: dbProduct
       };
@@ -22,8 +23,10 @@ module.exports = function (app) {
     });
   });
 
-  app.get("/grocery", function (req, res) {
-    db.Product.findAll({ raw: true, where: { category_id: 2 } }).then(function (dbProduct) {
+  app.get("/grocery", function(req, res) {
+    db.Product.findAll({ raw: true, where: { category_id: 2 } }).then(function(
+      dbProduct
+    ) {
       var hbsObject = {
         products: dbProduct
       };
@@ -32,8 +35,10 @@ module.exports = function (app) {
     });
   });
 
-  app.get("/books", function (req, res) {
-    db.Product.findAll({ raw: true, where: { category_id: 3 } }).then(function (dbProduct) {
+  app.get("/books", function(req, res) {
+    db.Product.findAll({ raw: true, where: { category_id: 3 } }).then(function(
+      dbProduct
+    ) {
       var hbsObject = {
         products: dbProduct
       };
@@ -42,8 +47,10 @@ module.exports = function (app) {
     });
   });
 
-  app.get("/clothing", function (req, res) {
-    db.Product.findAll({ raw: true, where: { category_id: 4 } }).then(function (dbProduct) {
+  app.get("/clothing", function(req, res) {
+    db.Product.findAll({ raw: true, where: { category_id: 4 } }).then(function(
+      dbProduct
+    ) {
       var hbsObject = {
         products: dbProduct
       };
@@ -52,8 +59,10 @@ module.exports = function (app) {
     });
   });
 
-  app.get("/outdoors", function (req, res) {
-    db.Product.findAll({ raw: true, where: { category_id: 5 } }).then(function (dbProduct) {
+  app.get("/outdoors", function(req, res) {
+    db.Product.findAll({ raw: true, where: { category_id: 5 } }).then(function(
+      dbProduct
+    ) {
       var hbsObject = {
         products: dbProduct
       };
@@ -62,27 +71,20 @@ module.exports = function (app) {
     });
   });
 
-  app.get("/automotive", function (req, res) {
-    db.Product.findAll({ raw: true, where: { category_id: 6 } }).then(function (dbProduct) {
+  app.get("/automotive", function(req, res) {
+    db.Product.findAll({ raw: true, where: { category_id: 6 } }).then(function(
+      dbProduct
+    ) {
       var hbsObject = {
         products: dbProduct
       };
       // console.log(dbProduct)
       res.render("items", hbsObject);
     });
-
-    db.Product.findAll({raw: true}).then(function(dbProduct){
-      var hbsObject = {
-        products: dbProduct
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
-
   });
 
-  app.get("/item/:id", function (req, res) {
-    db.Product.findOne({ where: { id: req.params.id } }).then(function (
+  app.get("/item/:id", function(req, res) {
+    db.Product.findOne({ where: { id: req.params.id } }).then(function(
       dbProduct
     ) {
       res.render("item", {
@@ -96,7 +98,7 @@ module.exports = function (app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.render("404");
   });
 };
