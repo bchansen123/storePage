48d1af5ffc1f625536d5263bece5b95b0c5ea27c
Max Power <dbroncosfan@gmail.com>
2020-02-12 12:48:15 -0700

    commit to pull

---
+++ models/userseeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (email, password, first_name, last_name, address, city, state, zipcode)
+VALUES
+("dbroncosfan@gmail.com", "root", "Curtis", "Allen", "123 Test Lane", "Logan", "Utah", 84321);
